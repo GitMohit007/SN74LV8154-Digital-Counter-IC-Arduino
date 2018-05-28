b5fca2e1f55cb16b56a066e9ca17fe4bcde04d02
Qubit1028 <39470632+Qubit1028@users.noreply.github.com>
2018-05-27 16:23:23 -0800

    Update counter.ino

--- counter.ino
+++ counter.ino
@@ -1,203 +1,177 @@
 #include "Arduino.h"
-#include <vector>
-#include <cstdlib>
-#include <iostream>
-#include <string>
-#include <sstream>
-#include "WString.h"
-#include <cmath>
-#include <algorithm>
-#include <sstream>
 
+//Flag for counting mode
+//Set to True for two 16 bit counters
+//Set to False for a single 32 bit counter
+int dual = 1;
 
-
-// Define Arduino I/O Pins
-int Y0 = 53;
-int Y1 = 51;
-int Y2 = 49;
-int Y3 = 47;
-int Y4 = 45;
-int Y5 = 43;
-int Y6 = 41;
-int Y7 = 39;
-int CCLR = 37;
-int GAL = 29;
-int GAU = 27;
-int GBL = 25;
-int GBU = 23;
-int RCLK = 31;
-int CLKBEN = 22;
-
-//Initialize variables
-int LowerA[8] = {0};
-int UpperA[8] = {0};
-int LowerB[8] = {0};
-int UpperB[8] = {0};
-int i;
-int reg;
+int freq = 5000;      //Frequency to read data in ms
 int dt = 0;
 
-std::vector<int> reg_byte(8);
-std::vector<int> LowA(8);
-std::vector<int> UpA(8);
-std::vector<int> LowB(8);
-std::vector<int> UpB(8);
+//************************************************
+//SN54HC165 Parallel to Serial Shift Register Pins
+//************************************************
+
+//SH/LD -- Pin 1 on IC
+int latchPin = 51;    //Set high to latch parallel data
+
+//QH -- Pin 9 on IC
+int dataPin = 49;     //Serial Output from IC
+
+//CLK -- Pin 2 on IC
+int clkPin = 53;      //Rising edge shifts bits
+
+//CLKINH -- Pin 15 on IC (optional)
+//  int clk_inhPin = 29;  //Set high to inhibit clocking
+
+//*************************************************
+//SN74LV8154 Dual 16 bit Counters
+//*************************************************
+
+//CCLR -- Pin 11 on IC
+int CCLR = 31;        //Set low to clear register
+
+//GAL -- Pin 3 on IC
+int GAL = 29;         //Gate A Lower Byte
+
+//GAU -- Pin 4 on IC
+int GAU = 27;		  //Gate A Upper Byte
+
+//GBL -- Pin 5 on IC
+int GBL = 25;		  //Gate B Lower Byte
+
+//GBU -- Pin 6 on IC
+int GBU = 23;         //Gate B Upper Byte
+
+//Initialize byte variables
+byte LowA = 0;
+byte HighA = 0;
+byte LowB = 0;
+byte HighB = 0;
+uint16_t CNT_A = 0;
+uint16_t CNT_B = 0;
+uint32_t CNT_TOT = 0;
 
 
+byte shiftIn(int data_pin, int clk_pin, int latch_pin, int gate) {
+	//Function retrieves byte from 'gate' on SN74LV8154
+	//Transfers byte onto shift register of SN54HC165
+	//Arduino then reads shift register and returns byte
+	byte data_in = 0;
+	int i;
+	byte temp = 0;
 
-void setup() {
+	//Select gate on counter IC
+	digitalWrite(gate, LOW);
+	delayMicroseconds(2);
 
-  Serial.begin(9600);
+	//Transfer parallel data from counter IC to shift register
+	digitalWrite(latch_pin, LOW);
+	delayMicroseconds(20);
+	digitalWrite(latch_pin, HIGH);
+	delayMicroseconds(20);
 
-  // Define input pins
-  pinMode(Y0, INPUT);    // Y0  Data Bit 0
-  pinMode(Y1, INPUT);    // Y1  Data Bit 1
-  pinMode(Y2, INPUT);    // Y2  Data Bit 2
-  pinMode(Y3, INPUT);    // Y3  Data Bit 3
-  pinMode(Y4, INPUT);    // Y4  Data Bit 4
-  pinMode(Y5, INPUT);    // Y5  Data Bit 5
-  pinMode(Y6, INPUT);    // Y6  Data Bit 6
-  pinMode(Y7, INPUT);    // Y7  Data Bit 7
+	for (i=0; i<8; i++) {
+	//IMPORTANT! First bit on shift register does not have to be clocked!
+		if (i == 0) {
+			temp = digitalRead(data_pin);
 
-  // Define output pins
-  pinMode(CCLR, OUTPUT);     // CCLR  Clears All Registers (hold low for > 20 ns)
-  pinMode(GAL, OUTPUT);     // GAL  Lower byte in A storage (hold low for Y)
-  pinMode(GAU, OUTPUT);     // GAU  Upper byte in A storage (hold low for Y)
-  pinMode(GBL, OUTPUT);     // GBL  Lower byte in B storage (hold low for Y)
-  pinMode(GBU, OUTPUT);     // GBU  Upper byte in B storage (hold low for Y)
-  pinMode(RCLK, OUTPUT);
-  pinMode(CLKBEN, OUTPUT);
-
-  //Set initial state
-  digitalWrite(CCLR, HIGH);
-  digitalWrite(GAL, HIGH);
-  digitalWrite(GAU, HIGH);
-  digitalWrite(GBL, HIGH);
-  digitalWrite(GBU, HIGH);
-  digitalWrite(RCLK, LOW);
-  digitalWrite(CLKBEN, LOW);
-
-}
-
-
-//Data retrieval from 16-bit counter IC
-void getData(int reg, std::vector<int> & v){
-    
-    //Transfer byte to output bus
-    digitalWrite(reg, LOW);
-    delayMicroseconds(30);
-
-    //Read byte
-    int b1 = digitalRead(Y0);
-    v[7] = b1;
-    int b2 = digitalRead(Y1);
-    v[6] = b2;
-    int b3 = digitalRead(Y2);
-    v[5] = b3;
-    int b4 = digitalRead(Y3);
-    v[4] = b4;
-    int b5 = digitalRead(Y4);
-    v[3] = b5;
-    int b6 = digitalRead(Y5);
-    v[2] = b6;
-    int b7 = digitalRead(Y6);
-    v[1] = b7;
-    int b8 = digitalRead(Y7);
-    v[0] = b8;
-	
-    //Release byte from output bus
-    digitalWrite(reg, HIGH);
-    delayMicroseconds(30);
-    
-
-}
-
-std::string comb_bytes(std::vector<int> &upA, std::vector<int> &lowA, std::vector<int> &upB,
-		                std::vector<int> &lowB) {
-	int num = 32;
-
-	std::vector<int> comb(num);
-
-	for (int i = 0; i < num; i++) {
-		if (i < 8) {
-			comb[i] = upB[i];
-		}
-		else if (i >= 8 && i < 16) {
-			comb[i] = lowB[i-8];
-		}
-		else if (i >= 16 && i < 24) {
-			comb[i] = upA[i-16];
+			data_in = data_in | temp;
 		}
 		else {
-			comb[i] = lowA[i-24];
+			digitalWrite(clk_pin, HIGH);
+			delayMicroseconds(10);
+			digitalWrite(clk_pin, LOW);
+			delayMicroseconds(10);
+			temp = digitalRead(data_pin);
+
+			data_in = (data_in << 1) | temp;
 		}
+
 	}
 
-	std::ostringstream ss;
-	for (std::vector<int>::iterator it = comb.begin(); it!=comb.end(); it++) {
-		ss << *it;
-	}
-    	std::string byte1 = ss.str();
-
-	return byte1;
-}
-
-void print_byte(std::vector<int> &b) {
-	for (size_t i = 0; i < b.size(); i++) {
-		Serial.print(b[i]);
-	}
+	/*For Debugging
 	Serial.println();
+	Serial.print("Gate: ");
+	Serial.print(gate);
+	Serial.print("  ");
+	Serial.print("SR Byte: ");
+	Serial.print(data_in);
+	Serial.print("  ");
+	Serial.println();
+	Serial.println();
+	digitalWrite(gate, HIGH);
+	delayMicroseconds(0.2);*/
+
+	return data_in;
 }
 
-// the loop routine runs over and over again forever:
-void loop() {
-
-  dt += 5;
-  
-  //Read Lower A Byte
-  getData(GAL, reg_byte);
-  LowA = reg_byte;
-  //Read Upper A Byte
-  getData(GAU, reg_byte);
-  UpA = reg_byte;
-  //Read Lower B Byte
-  getData(GBL, reg_byte);
-  LowB = reg_byte;
-  //Read Upper B Byte
-  getData(GBU, reg_byte);
-  UpB = reg_byte;
-
-  //Combine bytes
-  std::string byte1 = comb_bytes(UpA, LowA, UpB, LowB);
-  char * end;
-  const char * b1 = byte1.c_str();
-  //Convert to integer
-  long int val = std::strtol(b1,&end,2);
-
-  //Print to console
-  Serial.println("=========== Time (sec) ===========");
-  Serial.println(dt);
-  Serial.print("GAL: ");
-  print_byte(LowA);
-  Serial.print("GaU: ");
-  print_byte(UpA);
-  Serial.print("GBL: ");
-  print_byte(LowB);
-  Serial.print("GBU: ");
-  print_byte(UpB);
 
 
-  Serial.println();
-  Serial.print("Byte: ");
-  Serial.println(b1);
-  Serial.print("Count: ");
-  Serial.println(val);
-  Serial.println();
+void setup()
+{
+	Serial.begin(9600);
+	//Set pin modes
+	pinMode(latchPin, OUTPUT);
+	pinMode(dataPin, INPUT);
+	pinMode(clkPin, OUTPUT);
+	pinMode(GAL, OUTPUT);
+	pinMode(GAU, OUTPUT);
+	pinMode(GBL, OUTPUT);
+	pinMode(GBU, OUTPUT);
+	pinMode(CCLR, OUTPUT);
+	//pinMode(clk_inhPin, OUTPUT);
 
-  Serial.println("===================================");
-  Serial.println();
-  
-  //Delay
-  delay(5000);
+	//Set initial pin values
+	digitalWrite(latchPin, HIGH);
+	digitalWrite(clkPin, LOW);
+	digitalWrite(GAL, HIGH);
+	digitalWrite(GAU, HIGH);
+	digitalWrite(GBL, HIGH);
+	digitalWrite(GBU, HIGH);
+	digitalWrite(CCLR, HIGH);
+	//digitalWrite(clk_inhPin, HIGH);
+
+}
+
+
+void loop()
+{
+
+	//Get Bytes from Counter
+	LowA = shiftIn(dataPin, clkPin, latchPin, GAL);
+	HighA = shiftIn(dataPin, clkPin, latchPin, GAU);
+	LowB = shiftIn(dataPin, clkPin, latchPin, GBL);
+	HighB = shiftIn(dataPin, clkPin, latchPin, GBU);
+	byte all_bytes[] = {HighB, LowB, HighA, LowA};
+
+	Serial.print("Time: ");
+	Serial.println(dt);
+
+	if (dual) {
+		CNT_A = (HighA << 8) | LowA;
+		CNT_B = (HighB << 8) | LowB;
+		Serial.print("Counter A: ");
+		Serial.println(CNT_A);
+		Serial.print("Counter B: ");
+		Serial.println(CNT_B);
+		Serial.println();
+	}
+	else {
+		for (int i=0; i<4; i++) {
+			CNT_TOT = CNT_TOT + all_bytes[i];
+			CNT_TOT <<= 8;
+		}
+
+	}
+		CNT_TOT >>= 8;
+	    Serial.print("Count: ");
+		Serial.println(CNT_TOT);
+		Serial.println();
+
+
+	dt += freq/1000;
+	delay(freq);
+	CNT_TOT = 0;
 
 }
